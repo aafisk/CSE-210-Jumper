96a774f73313aa153dacb79c994ce9342fee05fc
aafisk <axelfisk12@gmail.com>
2022-10-18 15:00:45 -0600

    completed the hint and jumper class

--- Hint.cs
+++ Hint.cs
@@ -1,7 +1,23 @@
 class Hint {
 
-    public Hint() {
+    private List<string> hint = new List<string>();
 
+    public Hint(string word) {
+        for(int i = 0; i < word.Length; i++) {
+            hint.Add("_ ");
+        }
     }
 
+    // Prints the current hint to the console.
+    public void DisplayHint() {
+        Console.WriteLine();
+        for(int i = 0; i < hint.Count; i++) {
+            Console.Write(hint[i]);
+        }
+    }
+
+    // Updates the hint based on the letter and index provided
+    public void UpdateHint(string letter, int i) {
+        hint[i] = letter.ToLower();
+    }
 }
--- Parachute.cs
+++ Parachute.cs
@@ -1,18 +1,21 @@
 class Parachute {
 
-    string parachute = @"     
- _____
-/_____\
-\     /
- \   /
-   O
-  /|\
-  / \
+  private List<string> parachute = new List<string>();
 
-^^^^^^^";
+  public Parachute() {
+    parachute.Add(@" _____ ");
+    parachute.Add(@"/_____\");
+    parachute.Add(@"\     /");
+    parachute.Add(@" \   / ");
+    parachute.Add(@"   O   ");
+    parachute.Add(@"  /|\  ");
+    parachute.Add(@"  / \  ");
+    parachute.Add(@"       ");
+    parachute.Add(@"^^^^^^^");
+  }
 
-    public Parachute() {
-
-    }
+  public void CutParachute() {
+    parachute.RemoveAt(0);
+  }
 
 }
